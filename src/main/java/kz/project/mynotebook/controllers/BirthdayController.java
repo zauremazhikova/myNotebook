1d8fd9645984e1182bc2b9cfa23b51fbb473b1f5
zauremazhikova <mazhikovaz@gmail.com>
2020-04-17 23:51:17 +0600

    .gitignore

--- src/main/java/kz/project/mynotebook/controllers/BirthdayController.java
+++ src/main/java/kz/project/mynotebook/controllers/BirthdayController.java
@@ -2,10 +2,8 @@ package kz.project.mynotebook.controllers;
 import kz.project.mynotebook.entities.Birthday;
 import kz.project.mynotebook.repositories.BirthdayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
 @RestController
@@ -49,9 +47,11 @@ public class BirthdayController {
         return birthdayJson;
     }
 
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> birthday){
 
+        return birthday;
 
-
-
+    }
 
 }
